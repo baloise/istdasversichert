a9863ff639ecba2da291ffacb954facc92e65d0b
Lennart <alvestad@gmail.com>
2016-09-16 15:23:35 +0200

    initial view "objekt erfassen"

---
+++ public/objektErfassen/objektErfassen.js
@@ -0,0 +1,43 @@
+'use strict';
+
+angular.module('myApp.view1', [ 'ngRoute' ])
+
+.config([ '$routeProvider', function($routeProvider) {
+	$routeProvider.when('/objektErfassen', {
+		templateUrl : 'objektErfassen/view1.html',
+		controller : 'View1Ctrl'
+	});
+} ])
+
+.controller('View1Ctrl', ['$scope', function($scope) {
+	$scope.newItem = {};
+	$scope.setFile = function(element) {
+		console.log(element);
+		var reader = new FileReader();
+		var file = element.files[0];
+		if (file) {
+			console.log("has file");
+			reader.readAsDataURL(file);
+			reader.addEventListener("loadend", function(r) {
+				// console.log(r.target.result);
+				$scope.newItem.image = r.target.result;
+				$scope.$digest();
+			})
+		}
+	}
+
+//	$("#form").submit(function(e) {
+//		var data = {};
+//		data.image = image;
+//		data.description = "test";
+//		$.post("http://192.168.1.124:3000/items", data, null, "json");
+//	})
+//
+//	$("#category-input").autocomplete({
+//		data : {
+//			"Apple" : null,
+//			"Microsoft" : null,
+//			"Google" : 'http://placehold.it/250x250'
+//		}
+//	});
+} ]);
